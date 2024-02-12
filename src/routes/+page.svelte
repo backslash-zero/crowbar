bc4bb747dd55bea11aafc01cc81ecce60076b145
Celestin <hello@celest.in>
2024-02-12 15:37:09 +0100

    layout and fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,60 @@
-<main class="px-4 font-goudy">
-	<div class="w-full flex flex-col items-center">
+<main class="px-4 w-full font-goudy">
+	<div class="w-full pt-4 flex flex-col items-center gap-8 h-[100dvh]">
 		<h1 class="text-7xl italic font-argent">Welcome to the Crowbar</h1>
-
-		<div class="font-goudy opacity-50 text-xl w-full flex flex-col gap-4">
-			<p>
-				The Crow Bar is interactive device tailored specifically to Corvids, aiming to lay the
-				ground work for further research into their sensory perception and cognitive abilities.
-			</p>
-			<p>
-				The device is designed to not only facilitate in-depth study of Corvids but also to be
-				user-friendly and easily replicable for widespread application.
-			</p>
-			<p>
-				The proposed <strong
-					class="decoration-wavy underline-offset-4 underline decoration-gray-800 decoration-1"
-					>open-source</strong
-				>
-				design is meant to leverage Corvid’s remarkable curiosity and problem-solving abilities. The
-				Design includes a two-step puzzle which acts as a threshold to discriminate other birds to interact
-				with the device.
-			</p>
-		</div>
+		<div class="w-full h-full items-stretch bg-red-300"></div>
+		<div class="absolute bottom-4 text-center font-argent italic animate-pulse">scroll down</div>
+	</div>
+	<div class="font-goudy py-4 text-xl w-full flex flex-col gap-4 md:max-w-screen-lg">
+		<h2 class="font-argent text-7xl">About</h2>
+		<p>
+			The Crow Bar is interactive device tailored specifically to Corvids, aiming to lay the ground
+			work for further research into their sensory perception and cognitive abilities.
+		</p>
+		<p>
+			The device is designed to not only facilitate in-depth study of Corvids but also to be
+			user-friendly and easily replicable for widespread application.
+		</p>
+		<p>
+			The propose open-source design is meant to leverage Corvid’s remarkable curiosity and
+			problem-solving abilities. The Design includes a two-step puzzle which acts as a threshold to
+			discriminate other birds to interact with the device.
+		</p>
+	</div>
+	<div class="font-goudy py-4 text-xl w-full flex flex-col gap-4 md:max-w-screen-lg">
+		<h2 class="font-argent text-7xl">How to</h2>
+		<p>blablabla</p>
+	</div>
+	<div class="font-goudy py-4 text-xl w-full flex flex-col gap-4 md:max-w-screen-lg">
+		<h2 class="font-argent text-7xl">Files</h2>
+		<p>file 1</p>
+		<p>file 2</p>
+		<p>file 3</p>
+	</div>
+	<div class="font-goudy py-4 text-xl w-full flex flex-col gap-4 md:max-w-screen-lg">
+		<h2 class="font-argent text-7xl">Credits</h2>
+		<p>
+			The Crowbar is a project by Finnegan Hardy and Célestin Meunier, made within the context of
+			the master's program Design & Computation, a join program between the University of Arts
+			Berlin and Technical University.
+		</p>
+		<p>
+			Thanks to everyone who helped on this project: Prof. Dr. Daniel D. Hromada for both his
+			technical support and his beautiful outlook on technology and every life forms
+		</p>
+		<p>
+			Everyone at <a
+				href="https://www.newpractice.net/study"
+				class="font-argent text-lg italic underline underline-offset-[6px]">Design & Computation</a
+			>
+		</p>
+	</div>
+	<div class="font-goudy py-4 text-xl w-full flex flex-col gap-4 md:max-w-screen-lg">
+		<h2 class="font-argent text-7xl">Contacts</h2>
+		<p>hello (at) celest.in</p>
 	</div>
 </main>
 
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.gray.100);
 	}
 </style>
